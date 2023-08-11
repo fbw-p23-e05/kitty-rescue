fc0250d05436ae1304c1c475b86462f4d27428bd
Roberto Galvez Meyer <139347219+RobertoGM5@users.noreply.github.com>
2023-08-11 15:20:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 const button = document.getElementById('btn');
 const input = document.getElementById('amount');
-const cat = document.getElementById('cat');
+
 
 function handleDonation() {
     const amount = input.value;
 
     if (amount == 0) {
         alert('Thanks for nothing, the cats are all sad :(');
-        document.body.style.background = 'red';
+        document.body.style.background = 'black';
     } else {
         alert(`Thanks for the donation of $ ${amount}!`);
     }
 }
 
+var bgToggle = true;
+var cat = document.getElementById("cat");
+
 function catClick() {
-    document.body.style.background = 'pink';
+    if (bgToggle){
+        document.body.style.background = 'pink';
+        bgToggle = false;
+    } else{
+        document.body.style.background = null;
+        bgToggle = true;
+    }
 }
 
-function moveCat() {
-    const maxX = window.innerWidth - cat.width;
-    const maxY = window.innerHeight - cat.height;
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
+var clicks = 0;
 
-    cat.style.left = randomX + "px";
-    cat.style.top = randomY + "px";
-}
-
-function toggleCatClass() {
-    cat.classList.toggle("rotate");
-}
-
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
 
 button.addEventListener('click', handleDonation);
 cat.addEventListener('click', catClick);
-cat.addEventListener('click', moveCat);
-cat.addEventListener('mouseover', toggleCatClass);
-cat.addEventListener('mouseout', toggleCatClass);
+
+
+
+
